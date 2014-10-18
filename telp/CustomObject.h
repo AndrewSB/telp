fe213a7bf08bbbee8dd9672971614bc86561fccd
AndrewSB <asbreckenridge@me.com>
2014-10-18 01:10:52 -0400

    init

---
+++ telp/CustomObject.h
@@ -0,0 +1,17 @@
+//
+//  CustomObject.h
+//  telp
+//
+//  Created by Andrew Breckenridge on 10/18/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomObject : NSObject
+
+@property (strong, nonatomic) id someProperty;
+
+- (void) someMethod;
+
+@end
---
+++ telp/CustomObject.m
@@ -0,0 +1,17 @@
+//
+//  CustomObject.m
+//  telp
+//
+//  Created by Andrew Breckenridge on 10/18/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+#import "CustomObject.h"
+
+@implementation CustomObject
+
+- (void) someMethod {
+    NSLog(@"SomeMethod Ran");
+}
+
+@end
