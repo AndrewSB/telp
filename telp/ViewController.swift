3f22e9883880b656ef44df0baa52cb80597ffefa
AndrewSB <asbreckenridge@me.com>
2014-10-18 06:56:30 -0400

    complete autolayout, work on Parse login

--- telp/ViewController.swift
+++ telp/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var email: String?
+    var password: String?
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func loginButtonWasHit(sender: AnyObject) {
+        email = emailTextField.text
+        password = passwordTextField.text
+        self.parseLogin()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +32,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func parseLogin() -> Void {
+        var newUser = PFUser()
+        newUser.email = email
+        newUser.username = newUser.email
+        newUser.password = password
+        
+        newUser.signUpInBackgroundWithBlock((succeeded: Bool, error: NSError) in {
+            if (!error) {
+                // You're logged in
+            } else {
+                var errorString = error.userInfo["error"]
+            }
+            
+            })
     }
 
 
