56ccc25fd28e58935cfab5ac8d720256b3657d42
AndrewSB <asbreckenridge@me.com>
2014-10-18 00:58:46 -0400

    Initial Commit

---
+++ telp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  telp
+//
+//  Created by Andrew Breckenridge on 10/18/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
